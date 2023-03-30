f2299efb0059184b82c64767317e5418f7ec3bce
guylhermee <guylhermee.b@hotmail.com>
2023-03-30 09:32:13 -0300

    adcionar usuario nos modelos e ajeitar detalhes

--- galeria/admin.py
+++ galeria/admin.py
@@ -8,7 +8,7 @@ class ListandoFotografias(admin.ModelAdmin):
     list_editable = ("publicada",)
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
-    list_filter = ("categoria",)
+    list_filter = ("categoria", "usuario",)
     list_per_page = 10
     
 admin.site.register(Fotografia, ListandoFotografias)
